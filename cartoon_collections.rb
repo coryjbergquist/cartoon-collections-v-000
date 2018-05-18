5defe5a10b8c6966544e0ed9763cf36eba235933
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-18 18:45:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,8 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  cheese_types.each {|x|
-    if array.include?(x)
+  array.each {|x|
+    if x == cheese_types
       return x
     end
     }
