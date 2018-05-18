353206b420c5a48fb92edce0a10e7d2a805df61b
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-18 18:24:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(names)
+  names.each.with_index(1) {|name, index| puts "#{index}. #{name}"}
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+
+array.map {|x| x.capitalize + "!"}
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+ 
+ array.any? {|x| x.length > 4}
+ 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  new_string = array.join" "
+    if new_string.include?(cheese_types)
+       x = array & cheese_types
+       x.join""
+       binding.pry
+    else 
+    nil
+ end
 end
